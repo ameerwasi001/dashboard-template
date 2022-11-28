cb218d5482f6eb32a9831869418a0ecdc920d1a7
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2022-11-28 04:23:40 -0800

    Search functionality

--- layouts/table.js
+++ layouts/table.js
@@ -13,12 +13,11 @@ const emitter = Emitter.get()
 export const TableView = ({listingType, fields, data, accessors}) => {
   const [filter, setFilter] = useState(() => () => true)
   const [users, setUsers] = useState(data)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     setUsers(data)
-    emitter.on('filter', (f) => {
-      setFilter(() => d => f(d))
-    })  
+    emitter.on('filter', f => setFilter(() => d => f(d))) 
   }, [])
 
   useEffect(() => {
@@ -34,32 +33,46 @@ export const TableView = ({listingType, fields, data, accessors}) => {
   }, [users])
 
   if(users === null) return 
-  return <div className="d-flex justify-content-center" style={{width: "100vw"}} key={users}>
-    <div className="table-responsive" style={{width: "80vw"}}>
-      <table className="table">
-        <caption style={{color: "white"}}>List of {listingType}</caption>
-        <thead>
-          <tr>
+  return <div>
+    <div className="d-flex justify-content-end align-items-center" style={{width: "90vw"}}>
+      <div>Search</div>
+      <div className="m-2"></div>
+      <input style={{width: "20vw"}} type="text" onInput={e => setSearch(e.target.value)} value={search} className="form-control" />
+    </div>
+    <br/>
+    <div className="d-flex justify-content-center" style={{width: "100vw"}}>
+      <div className='row' style={{ background: "whitesmoke", height: "1px" , width: "80vw"}}></div>
+    </div>
+    <br/>
+    <div className="d-flex justify-content-center" style={{width: "100vw"}} key={users}>
+      <div className="table-responsive" style={{width: "80vw"}}>
+        <table className="table">
+          <caption style={{color: "white"}}>List of {listingType}</caption>
+          <thead>
+            <tr>
+              {
+                  fields.map(field => <th scope="col" style={{color: "white"}}>{`${field}`}</th>)
+              }
+            </tr>
+          </thead>
+          <tbody>
             {
-                fields.map(field => <th scope="col" style={{color: "white"}}>{`${field}`}</th>)
+              users.filter(filter).filter(u => {
+                return JSON.stringify(u).toLowerCase().includes(search.toLowerCase())
+              }).map(user => {
+                  return <tr>
+                  {
+                      accessors.map(f => <th scope="col" style={{color: "white"}}>
+                              {f(user)}
+                          </th>
+                      )
+                  }
+                </tr>
+              })
             }
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.filter(filter).map(user => {
-                return <tr>
-                {
-                    accessors.map(f => <th scope="col" style={{color: "white"}}>
-                            {f(user)}
-                        </th>
-                    )
-                }
-              </tr>
-            })
-          }
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 }
